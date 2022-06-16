8d7b2c4306cad63304d7d5e6b0ccb24e45858583
311 <ismoilov92@gmail.com>
2022-06-16 23:28:46 +0300

    add webix frontend framework

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,10 @@
+webix.ready(function(){
+    webix.ui({
+        type:"line",
+        container:"app",
+        rows:[
+            { template:"View 1" },
+            { template:"View 2" }
+        ]
+    });
+});
