83e2e5127e51dd4785473c0026e163a924a5e354
311 <ismoilov92@gmail.com>
2022-06-19 19:32:58 +0300

    Added cars domain and controller

---
+++ src/main/java/com/example/logistic/controller/CarController.java
@@ -0,0 +1,16 @@
+package com.example.logistic.controller;
+
+import com.example.logistic.domain.Car;
+import com.example.logistic.repo.CarRepo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/car")
+public class CarController extends AbstractRestController<Car, CarRepo> {
+
+    public CarController(CarRepo repo) {
+        super(repo);
+    }
+}
---
+++ src/main/java/com/example/logistic/domain/Car.java
@@ -0,0 +1,20 @@
+package com.example.logistic.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+}
